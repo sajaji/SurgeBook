ae56bed43f4bcf355c1b37405e795ec3922c378d
Sajaji Sajith <mohamedsajaji@icloud.com>
2023-02-04 23:09:47 +0530

    modifications

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,21 +56,9 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          SurgeBook
         </Typography>
-        {isNonMobileScreens && (
-          <FlexBetween
-            backgroundColor={neutralLight}
-            borderRadius="9px"
-            gap="3rem"
-            padding="0.1rem 1.5rem"
-          >
-            <InputBase placeholder="Search..." />
-            <IconButton>
-              <Search />
-            </IconButton>
-          </FlexBetween>
-        )}
+        
       </FlexBetween>
 
       {/* DESKTOP NAV */}
@@ -83,9 +71,6 @@ const Navbar = () => {
               <LightMode sx={{ color: dark, fontSize: "25px" }} />
             )}
           </IconButton>
-          <Message sx={{ fontSize: "25px" }} />
-          <Notifications sx={{ fontSize: "25px" }} />
-          <Help sx={{ fontSize: "25px" }} />
           <FormControl variant="standard" value={fullName}>
             <Select
               value={fullName}
